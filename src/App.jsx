8371f32144e6ce98be07fc3c20d2c4c58718b76c
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-10-14 19:40:14 +0100

    add: Version 1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [colors, setColors] = useState({
+    colorA: randomColor(),
+    colorB: randomColor(),
+    colorC: randomColor(),
+  });
+
+  const handleGenerateColors = () => {
+    setColors({
+      colorA: randomColor(),
+      colorB: randomColor(),
+      colorC: randomColor(),
+    });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="w-80 h-auto bg-white p-4 rounded-lg shadow-md flex flex-col items-center gap-4">
+      <h1 className="text-xl font-bold text-gray-800">Shadify</h1>
+      <div className="flex w-full gap-2">
+        <div className='flex-1 rounded-lg shadow-md transition-all'>
+          <div className="h-20" style={{ backgroundColor: colors.colorA }}></div>
+          <p className="text-center text-sm font-medium text-gray-800 py-3">{colors.colorA}</p>
+        </div>
+        <div className='flex-1 rounded-lg shadow-md transition-all'>
+          <div className="h-20" style={{ backgroundColor: colors.colorB }}></div>
+          <p className="text-center text-sm font-medium text-gray-800 py-3">{colors.colorB}</p>
+        </div>
+        <div className='flex-1 rounded-lg shadow-md transition-all'>
+          <div className="h-20" style={{ backgroundColor: colors.colorC }}></div>
+          <p className="text-center text-sm font-medium text-gray-800 py-3">{colors.colorC}</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <button
+        className="w-full bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white font-medium py-2 rounded-lg transition-all"
+        onClick={handleGenerateColors}
+      >
+        Generate Colors
+      </button>
+    </main>
+  );
 }
 
-export default App
+export default App;
