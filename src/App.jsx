51e52640600c5fb3e668036046706f0e739e53a0
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-10-14 20:29:47 +0100

    add: Added the localstorage in 1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { ArrowUpRightFromSquare } from 'lucide-react';
 import randomColor from 'randomcolor';
 import { useState } from 'react';
 
 function App() {
+  const localColorA = localStorage.getItem("ColorA");
+  const localColorB = localStorage.getItem("ColorB");
+  const localColorC = localStorage.getItem("ColorC");
+
   const [colors, setColors] = useState({
-    colorA: randomColor(),
-    colorB: randomColor(),
-    colorC: randomColor(),
+    colorA: localColorA === null || localColorA === undefined ? randomColor() : localColorA,
+    colorB: localColorB === null || localColorB === undefined ? randomColor() : localColorB,
+    colorC: localColorC === null || localColorA === undefined ? randomColor() : localColorC,
   });
+
+  localStorage.setItem("ColorA", colors.colorA)
+  localStorage.setItem("ColorB", colors.colorB)
+  localStorage.setItem("ColorC", colors.colorC)
 
   const handleGenerateColors = () => {
     setColors({
@@ -16,9 +25,20 @@ function App() {
     });
   };
 
+  const handleFollow = () => {
+    window.open("https://www.instagram.com/shawaizkhan.dev/", "_blank");
+  };
+
   return (
     <main className="w-80 h-auto bg-white p-4 rounded-lg shadow-md flex flex-col items-center gap-4">
-      <h1 className="text-xl font-bold text-gray-800">Shadify</h1>
+      <div className='flex justify-start w-full items-center gap-3'>
+        <h1 className="text-xl font-bold text-gray-800">Shadify</h1>
+        <ArrowUpRightFromSquare
+          className='text-blue-500 cursor-pointer'
+          onClick={handleFollow}
+          size={23}
+        />
+      </div>
       <div className="flex w-full gap-2">
         <div className='flex-1 rounded-lg shadow-md transition-all'>
           <div className="h-20" style={{ backgroundColor: colors.colorA }}></div>
